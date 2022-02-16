cf25913e9ee2d6757dac64438f216670be1e2376
A.S.M. Hasan Sarker <asmhasansarker@gmail.com>
2022-02-16 19:29:09 +0600

    finish

---
+++ resources/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    // STICKY MENU
+    $(".js--services-section").waypoint(function(direction){
+
+        if(direction == "down"){
+                $("nav").addClass("sticky");
+        }
+        else{
+
+            $("nav").removeClass("sticky");
+
+        }
+
+
+
+    });
+       
+       
+       
+     // MIXITUP(PORTFOLIO SECTION)
+    var mixer = mixitup('.container');
+    // SMOOTH SCROLL FOR IE/ EDGE / SAFARI
+
+
+
+
+
+
+
+
+
+
+});
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
